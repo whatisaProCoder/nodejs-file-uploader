be52bdf5eb0c4f5aa7f4afad1890b0aa282a8832
whatisaProCoder <debnathpritam2005@gmail.com>
2026-04-10 21:02:10 +0530

    fix

--- public/script.js
+++ public/script.js
@@ -7,3 +7,20 @@ window.onscroll = () => {
     header.classList.remove("scrolled")
   }
 }
+
+// add new folder dialog box
+
+const addNewFolderButton = document.querySelector(".folders-section .new-button")
+const addNewFolderDialog = document.querySelector(".add-folder-dialog");
+const closeNewFolderDialog = document.querySelector(".close-dialog-button");
+
+addNewFolderButton.addEventListener("click", () => {
+  addNewFolderDialog.showModal();
+})
+
+closeNewFolderDialog.addEventListener("click", () => {
+  addNewFolderDialog.close();
+})
+
+
+
